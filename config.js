10efdcff68a2d31c886f6f0e5632c61e0b7c41ef
Hoàng Chí Thanh <93084511+HcThanh03@users.noreply.github.com>
2023-02-24 17:35:24 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Diễm Châu';
+const giftUrl = 'https://www.facebook.com/chau.diem.18400700';
+const eventName = 'Happy Birthday';
+const titleCard = 'Châu xinh đẹp';
+const contentCard = 'Chúc Châu sinh nhật vui vẻ, tuổi mới thật nhiều thành công và ngày càng xinh đẹp, ráng kiếm được any chăm sóc cho nhá. Nếu có đám cưới thì mời luôn cũng được hahaha.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'PDC.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
